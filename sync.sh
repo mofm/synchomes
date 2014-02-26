f54775120e4e287f89c619354a28aee1eeedcda5
Emre Eryilmaz <emre.eryilmaz@piesso.com>
2014-02-27 01:32:54 +0200

    modified:   sync.sh 	modified:   sync_exclude

--- sync.sh
+++ sync.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-EXCLUDE="$HOME/rsync_exclude"
-PORT="2212"
-HOST="piesso.com"
-USER="backup"
-SYNC_DIRECTORY="/home/backup/test/"
+# exclude-list file
+EXCLUDE=".sync_exclude"
+# port for rsync over ssh
+PORT="your_port"
+# backup hostname or IP
+HOST="examples.com"
+# username for rsync over ssh
+USER="user"
+# backup directory path on backup host
+SYNC_DIRECTORY="/home/user/backup/"
 
 rsync -avz --exclude-from "$EXCLUDE" --max-size=50M -e 'ssh -p '$PORT'' $HOME "$USER"@"$HOST":"$SYNC_DIRECTORY"
